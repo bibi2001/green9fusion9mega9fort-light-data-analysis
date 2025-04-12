18fee83f5cda67e3ad81598884948674c807676a
bibi2001 <bibivivid2001@gmail.com>
2025-04-13 03:00:12 +0900

    Update development log at 2025-04-13 03:00:12

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -171,3 +171,27 @@ def read_file_lines(filename):
 def write_file_lines(filename, lines):
     with open(filename, 'w') as file:
         file.writelines(lines)
+
+# Update at 2025-04-13 03:00:12
+# Optimized the algorithm
+# Fixed some bugs
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
