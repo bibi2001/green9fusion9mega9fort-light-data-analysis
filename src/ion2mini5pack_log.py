6cd9ed4d764d752f742c51f42c8c2d4de5445596
bibi2001 <bibivivid2001@gmail.com>
2025-04-20 05:43:06 +0900

    Update development log at 2025-04-20 05:43:05

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -306,3 +306,27 @@ def save_to_json(data, filename):
 def load_from_json(filename):
     with open(filename, 'r') as f:
         return json.load(f)
+
+# Update at 2025-04-20 05:43:05
+# Added unit tests
+# Added documentation
+# Added error handling
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
