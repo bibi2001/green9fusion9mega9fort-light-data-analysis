962190a6f0636a58dccb89b19071c5ec7d61d1e1
bibi2001 <bibivivid2001@gmail.com>
2025-04-28 03:25:10 +0900

    Update development log at 2025-04-28 03:25:10

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -444,3 +444,16 @@ def binary_search(arr, target):
         else:
             right = mid - 1
     return -1
+
+# Update at 2025-04-28 03:25:10
+# Optimized the algorithm
+
+import json
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_from_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
