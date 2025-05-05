25c5a00dda2eb69ffa6a36c4c68f33f856f3d439
bibi2001 <bibivivid2001@gmail.com>
2025-05-05 09:15:10 +0900

    Update development log at 2025-05-05 09:15:09

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -563,3 +563,18 @@ class Observer:
     def notify(self):
         for observer in self._observers:
             observer.update()
+
+# Update at 2025-05-05 09:15:09
+# This is a random comment
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
