2cf2688c02f4939137b012dbf79e9d07ee1b69ff
bibi2001 <bibivivid2001@gmail.com>
2025-05-12 01:02:10 +0900

    Update development log at 2025-05-12 01:02:09

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -668,3 +668,14 @@ class Observer:
     def notify(self):
         for observer in self._observers:
             observer.update()
+
+# Update at 2025-05-12 01:02:09
+# Fixed some bugs
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
