e092e6314eeefc9168467f1c040b9b0ad6f00faf
bibi2001 <bibivivid2001@gmail.com>
2025-05-16 23:53:04 +0900

    Update development log at 2025-05-16 23:53:02

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -735,3 +735,27 @@ def factorial(n):
     if n == 0 or n == 1:
         return 1
     return n * factorial(n-1)
+
+# Update at 2025-05-16 23:53:02
+# Updated the code with new features
+# Refactored the code
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
