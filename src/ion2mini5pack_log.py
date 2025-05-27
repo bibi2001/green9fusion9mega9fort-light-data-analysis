a4706a3c195bd4ac6a725aef0908ea2caa597c93
bibi2001 <bibivivid2001@gmail.com>
2025-05-27 10:44:08 +0900

    Update development log at 2025-05-27 10:44:07

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -917,3 +917,19 @@ def read_file_lines(filename):
 def write_file_lines(filename, lines):
     with open(filename, 'w') as file:
         file.writelines(lines)
+
+# Update at 2025-05-27 10:44:07
+# Refactored the code
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
