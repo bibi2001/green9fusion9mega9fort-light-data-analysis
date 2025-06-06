27790b5b52bb5aa9a093319a83454baa7444185a
bibi2001 <bibivivid2001@gmail.com>
2025-06-07 07:09:08 +0900

    Update development log at 2025-06-07 07:09:07

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -1082,3 +1082,19 @@ def binary_search(arr, target):
         else:
             right = mid - 1
     return -1
+
+# Update at 2025-06-07 07:09:07
+# Optimized the algorithm
+# Added documentation
+# Improved performance
+
+def read_file_lines(filename):
+    try:
+        with open(filename, 'r') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return []
+
+def write_file_lines(filename, lines):
+    with open(filename, 'w') as file:
+        file.writelines(lines)
