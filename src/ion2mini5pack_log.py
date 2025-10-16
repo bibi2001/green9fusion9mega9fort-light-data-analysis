c704af467f3fd11401ac97d3d176dfbef54e1b6d
bibi2001 <bibivivid2001@gmail.com>
2025-10-17 03:42:13 +0900

    Update development log at 2025-10-17 03:42:12

--- src/ion2mini5pack_log.py
+++ src/ion2mini5pack_log.py
@@ -1513,3 +1513,27 @@ class Observer:
     def notify(self):
         for observer in self._observers:
             observer.update()
+
+# Update at 2025-10-17 03:42:12
+# Updated the code with new features
+# Fixed some bugs
+# Added unit tests
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
